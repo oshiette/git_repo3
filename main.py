742a4e9ee79307a510ee50be88ba49f91310e4df
201-06 <student@201-06>
2022-11-05 13:31:51 +0300

    добавили суперфункцию

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+def my_superfunction():
+    print("Много принтов")
 def main ():
     print ("Первый принт")
     print("Второй принт")
     print("еще принт")
     print("Сижу в поезде и много думаю")
-
+    my_superfunction()
 if __name__ == '__main__':
     main()
